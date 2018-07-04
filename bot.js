476bad8033c749bb3c525cb5655182be1076d4ba
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-04 09:55:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,30 @@ client.login(process.env.BOT_TOKEN);
 
 
 
-
+  client.on('message', ReBeeL => {
+  var prefix = "$";
+    if(ReBeeL.author.bot) return;
+      if(ReBeeL.content.startsWith(prefix + "bcowner")) {
+        let args = ReBeeL.content.split(" ").slice(1);
+           if(!args[0]) {
+              ReBeeL.channel.send("** $bcowner <message> **")
+                return;
+                  }      
+                   var rebel = new Discord.RichEmbed()
+                      .setColor("#000000")
+                        .setDescription(`
+تم إرسآل لك رسآلة من السيرفر الخاص بك 
+${ReBeeL.guild.name}
+الرسآلة 
+${args}
+        `)
+        .setFooter(` بوآسطة ${ReBeeL.author.username}#${ReBeeL.author.discriminator}`)
+       ReBeeL.guild.owner.send(rebel);
+      ReBeeL.channel.send("**تم إرسآل الرسآلة إلى أونر السيرفر**")
+     }
+    }
+  );
+  
 
 
 
