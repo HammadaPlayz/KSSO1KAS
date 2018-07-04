02553c52b111a52aeed87f47e723350ff412df56
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-04 13:16:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,34 @@ client.login(process.env.BOT_TOKEN);
 
 
 
-
+var prefix = "$";
+const sql = require("sqlite");
+client.on("message", async message => {
+    if (message.content.startsWith(prefix + "angaz")) {
+         var ids = [
+            "20",
+            "1",
+            "13",
+            "18",
+            "17",
+            "9",
+            "31",
+            "22",
+            "23",
+            "2",
+            "11",
+            "19",
+            "24",
+            "25",
+            "12",
+            "33"
+            ]
+            const randomizer = Math.floor(Math.random()*ids.length);
+            const args = message.content.split(" ").slice(1).join(" ")
+    if (!args) return message.channel.send("**اكتب محتوي الانجاز**");
+    const image = new Discord.Attachment(`https://www.minecraftskinstealer.com/achievement/a.php?i=${ids[randomizer]}&h=Achievement Get!&t=${args}`, "achievement.png");
+message.channel.send(image)
+    }
 
 
   client.on('message', ReBeeL => {
