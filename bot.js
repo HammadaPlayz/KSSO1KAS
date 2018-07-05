e632bab658932de99ded2588b5a7de051379528b
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-05 05:11:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '$'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help | ${client.guilds.size} Servers  | NoobBot 0.2  `,"http://twitch.tv/Mohamed192837465")
+client.user.setGame(`$help | $invite | ${client.guilds.size} Servers`,"http://twitch.tv/Mohamed192837465")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -47,12 +47,13 @@ client.login(process.env.BOT_TOKEN);
 
 
 
-// const Canvas = require("canvas-prebuilt"); // اذا كنت وندوز
-// npm i canvas-prebuilt
 const fs = require("fs"); // npm i fs
 const Canvas = require("canvas"); //npm i canvas
 const jimp = require("jimp"); //npm i jimp
 const moment = require("moment") //npm i moment
+// const Canvas = require("canvas-prebuilt"); // اذا كنت وندوز
+// npm i canvas-prebuilt
+
 
 let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
 client.on("message", message => {
@@ -60,7 +61,7 @@ client.on("message", message => {
   if (message.author.bot) return;
   if(!message.channel.guild)return;
   if (!profile[message.author.id]) profile[message.author.id] = {
-    tite: 'New User',
+    tite: 'Noobbot',
     rep: 0,
     reps: 'NOT YET',
     lastDaily:'Not Collected',
@@ -81,12 +82,12 @@ client.on('message', message => {
       if(!message.channel.guild) return;
                     moment.locale('en');
                   var getvalueof = message.mentions.users.first()
-                    if(!getvalueof) return message.channel.send(`**:mag: |  ${message.author.username}, the user could not be found.    **`);
-                       if(getvalueof.id == message.author.id) return message.channel.send(`**${message.author.username}, you cant give yourself a reputation !**`)
+                    if(!getvalueof) return message.channel.send(`**:mag: |  ${message.author.username}, لا يوجد    **`);
+                       if(getvalueof.id == message.author.id) return message.channel.send(`**${message.author.username}, لا ينكن اعطاء نفسك تقييم !**`)
     if(profile[message.author.id].reps != moment().format('L')) {
             profile[message.author.id].reps = moment().format('L');
             profile[getvalueof.id].rep = Math.floor(profile[getvalueof.id].rep+1);
-         message.channel.send(`** :up:  |  ${message.author.username} has given ${getvalueof} a reputation point!**`)
+         message.channel.send(`** :up:  |  ${message.author.username} اعطا ${getvalueof} تقييم**`)
         } else {
          message.channel.send(`**:stopwatch: |  ${message.author.username}, you can raward more reputation  ${moment().endOf('day').fromNow()} **`)
         }
@@ -120,7 +121,7 @@ if(message.content.startsWith("$daily")) {
   if(profile[message.author.id].lastDaily != moment().format('day')) {
     profile[message.author.id].lastDaily = moment().format('day')
     profile[message.author.id].credits += 200
-     message.channel.send(`**${message.author.username} انت اخذت \`200\` :dollar: daily pounds**`)
+     message.channel.send(`**${message.author.username} جمعت \`200\` :dollar: daily pounds**`)
 } else {
     message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
 }
@@ -129,20 +130,20 @@ if(message.content.startsWith("$daily")) {
  let cont = message.content.slice(prefix.length).split(" ");
 let args = cont.slice(1);
 let sender = message.author
-if(message.content.startsWith('$credits')) {
+if(message.content.startsWith('$trans')) {
           if (!args[0]) {
-            message.channel.send(`**$credits [name] [عدد]**`);
+            message.channel.send(`**$trans [name] [عدد]**`);
          return;
            }
         // We should also make sure that args[0] is a number
         if (isNaN(args[0])) {
-            message.channel.send(`**$credits [name] [عدد]**`);
+            message.channel.send(`**$trans [name] [عدد]**`);
             return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
              }
             let defineduser = '';
             let firstMentioned = message.mentions.users.first();
             defineduser = (firstMentioned)
-            if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+            if (!defineduser) return message.channel.send(`**$trans [name] [عدد]**`);
             var mentionned = message.mentions.users.first();
 if (!profile[sender.id]) profile[sender.id] = {}
 if (!profile[sender.id].credits) profile[sender.id].credits = 200;
@@ -155,7 +156,7 @@ if (err) console.error(err);
       profile[defineduser.id].credits += (+args[0]);
       profile[sender.id].credits += (-args[0]);
       let mariam = message.author.username
-message.channel.send(`**:moneybag: | ${message.author.username}, عطا ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
+message.channel.send(`**:moneybag: | ${message.author.username}, اعطا ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
 }
  
       });
@@ -170,7 +171,7 @@ message.channel.send(`**:moneybag: | ${message.author.username}, عطا ` + "`" 
           if(profile[message.author.id].points > 100) {
               profile[message.author.id].points = 0
               profile[message.author.id].level = Math.floor(profile[message.author.id].level+1);
-              message.channel.send(`**${message.author.username}, انت في لفل __${profile[message.author.id].level}__**`)
+              message.channel.send(`**${message.author.username}, لفلك صار __${profile[message.author.id].level}__**`)
           }
           fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
 if (err) console.error(err);
@@ -180,12 +181,12 @@ if (err) console.error(err);
     client.on('message', message => {
         let tit = message.content.split(" ").slice(1).join(" ");
         if(message.content.startsWith("$title")) {
-        if(!profile[message.author.id].tite) profile[message.author.id].tite = "NoobBot"
+        if(!profile[message.author.id].tite) profile[message.author.id].tite = "Hey im using Galaxy"
         if(!tit) {
-            message.channel.send("**$title [ كلام ]**");
+            message.channel.send("**$title [شيء]**");
         } else {
             profile[message.author.id].tite = tit
-            message.channel.send(`:ok: `)
+            message.channel.send(`Done ! | Noobbot`)
         }
         }
         fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
@@ -232,7 +233,7 @@ if (!profile[getvalueof.id]) profile[getvalueof.id] = {points: 0,reps: "NOT YET"
             let Image = Canvas.Image,
             canvas = new Canvas(300, 300),
             ctx = canvas.getContext('2d');
-            fs.readFile("./hlcpro.png", function (err, Background) { //امتداد الصورة
+            fs.readFile("./hlcpro.lnk", function (err, Background) { //امتداد الصورة
             if (err) return console.log(err);
             let BG = Canvas.Image;
             let ground = new Image;
@@ -363,6 +364,7 @@ $members | عدد الاعضاء وحالتهم
 $rep |تقييم
 $daily| المصروف
 $profile | بروفايل
+$tran | تحويل كريدت
 $credit | كريدت
 $ping | بنق
 $angas | انجاز ماينكرفتي
