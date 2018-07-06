a4ac35c7a0a765420637b8bb388193ad7a53ad6e
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-06 15:32:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -466,9 +466,7 @@ $كت تويت
 $لو خيروك
 $rps [حجر - ورقه - مقص]
 $عقاب 
-$مريم 
-$فكك 
-$عواصم
+$مريم
 سيرفر السبورت : https://discord.gg/9bYFkab
 `);
        }
@@ -1004,91 +1002,7 @@ client.on('guildMemberAdd', member => {
 
 	
 
-client.on('message', message => {
 
-if (message.content.startsWith(prefix + 'فكك')) { 
-    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-
-const type = require('./fakk.json'); 
-const item = type[Math.floor(Math.random() * type.length)]; 
-const filter = response => { 
-    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-message.channel.send('**لديك 15 ثانيه لتفكيك الجمله **').then(msg => {
-    let embed = new Discord.RichEmbed()
-    .setColor('#000000')
-    .setFooter("فكك  | NoobBot", 'https://cdn.discordapp.com/avatars/439427357175185408/3eb163b7656922ebc9e90653d50231f1.png?size=2048')
-    .setDescription(`**قم بتفكيك الجمله : ${item.type}**`) // ${item.type} = السؤال
-
-    msg.channel.sendEmbed(embed).then(() => {
-        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-        .then((collected) => {
-        message.channel.send(`${collected.first().author} ✅ **الاجابه صحيحه**`);
-        console.log(`[Typing] ${collected.first().author} typed the word.`);
-            let won = collected.first().author; 
-            points[won.id].points++;
-          })
-          .catch(collected => { 
-            message.channel.send(`:x: *لم يقم احد بكتابه الكلمه مفككه صحيح**`);
-            console.log(`[Typing] Error: No one type the word.`);
-          })
-        })
-    })
-}
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-			
-
-
-
-	
-
-
-
-	
-client.on('message', message => {
-if (message.content.startsWith(prefix + 'عواصم')) { 
-    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-
-const type = require('./3wasem.json'); 
-const item = type[Math.floor(Math.random() * type.length)]; 
-const filter = response => { 
-    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-message.channel.send('**لديك 15 ثانية لتوجد عاصمة **').then(msg => {
-    let embed = new Discord.RichEmbed()
-    .setColor('#000000')
-    .setFooter("عواصم  | NoobBot", 'https://cdn.discordapp.com/avatars/439427357175185408/3eb163b7656922ebc9e90653d50231f1.png?size=2048')
-    .setDescription(`**قم بكتابة عاصمة : ${item.type}**`) // ${item.type} = السؤال
-
-    msg.channel.sendEmbed(embed).then(() => {
-        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-        .then((collected) => {
-        message.channel.send(`${collected.first().author} ✅ **الاجابه صحيحه**`);
-        console.log(`[Typing] ${collected.first().author} typed the word.`);
-            let won = collected.first().author; 
-            points[won.id].points++;
-          })
-          .catch(collected => { 
-            message.channel.send(`:x: **لم يقم أحد بكتابة العاصمة بالوقت المناسب**`);
-            console.log(`[Typing] Error: No one type the word.`);
-          })
-        })
-    })
-}
-});
 	
 	
 
