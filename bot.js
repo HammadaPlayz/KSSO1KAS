069c59649bcf81a24b59d4fb820b927d7addc10b
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-07 05:47:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2090,12 +2090,7 @@ client.on('message', function(msg) {
   
   
   
-  
-  
-  
-  
-  
-  
+ 
   
   
   
@@ -2104,50 +2099,6 @@ client.on('message', function(msg) {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  client.on('message' , async (message) => {
-		  var prefix = "$"
- if (message.content.startsWith(prefix + 'servers')) {
-    // Lets define our array of guilds
-    const guildArray = client.guilds.map((guild) => {
-    return `${guild.name} : ${guild.count}` ${guild.owner}`
-    })
-  
-    // And send it
-    message.channel.send(`\`\`\`${guildArray.join("\n")}\`\`\``)
-  }
-});
-
 
 
 
