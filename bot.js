cf9185ec8a77fc4e48406b8de01c10540d13baa1
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-16 02:40:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,62 +31,14 @@ client.user.setGame(`$help | $inv | ${client.guilds.size} Servers `,"http://twit
 client.login(process.env.BOT_TOKEN);
 
 
-client.on('message' , async (message) => {
-       if(message.content.startsWith(`Noobbot`)) {
-              
- let responses = [
-        'جرب $help ممكن يساعدك',
-        'هلا ولله وش فيك تمنشني؟ ',
-        'حد ناداني؟',
-    ]
-    
-    // Fetch a random item from the array
-    let fetched = responses[Math.floor(Math.random() * responses.length)];
-   message.reply(fetched)
-       }
-  
-});
 
-
-client.login(process.env.BOT_TOKEN);
-
-
-client.on('message' , async (message) => {
-       if(message.content.startsWith(`NoobBot`)) {
-              
- let responses = [
-        'جرب $help ممكن يساعدك',
-        'هلا ولله وش فيك تمنشني؟ ',
-        'حد ناداني؟',
-    ]
-    
-    // Fetch a random item from the array
-    let fetched = responses[Math.floor(Math.random() * responses.length)];
-   message.reply(fetched)
-       }
-  
-});
 
 
 
 client.login(process.env.BOT_TOKEN);
 
 
-client.on('message' , async (message) => {
-       if(message.content.startsWith(`noobbot`)) {
-              
- let responses = [
-        'جرب $help ممكن يساعدك',
-        'هلا ولله وش فيك تمنشني؟ ',
-        'حد ناداني؟',
-    ]
-    
-    // Fetch a random item from the array
-    let fetched = responses[Math.floor(Math.random() * responses.length)];
-   message.reply(fetched)
-       }
-  
-});
+
 
  
 
