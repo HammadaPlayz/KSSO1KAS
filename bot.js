b7e71e95aa900457a3740224b8120baaf9f99f6e
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-20 22:54:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,21 @@ client.user.setGame(`$help | $inv ${client.guilds.size} Servers ${client.users.s
 client.login(process.env.BOT_TOKEN);
 
 
+const child_process = require("child_process");
+const adminprefix = "admin$";
+const devs = ['452292328569307137'];
+
+client.on('message', message => {
+if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
+        console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(تم اعادة تشغيل البوت);
+    }
+
+  }); // By Ghost
 
 
 client.on('message', message => {
