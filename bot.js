516ed9c5ce024c27813230485f8c072d94619d8a
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-20 14:29:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,14 +37,14 @@ client.login(process.env.BOT_TOKEN);
 
 client.on('message', message => {
     if(message.channel.type === "dm") return;
-      if(message.content.startsWith ("$marry")) {
+      if(message.content.startsWith ("+زواج")) {
       if(!message.channel.guild) return message.reply(' This command only for servers ')
       var proposed = message.mentions.members.first()
 
-      if(!message.mentions.members.first()) return message.reply(`لازم تطلب ايد وحدة`).catch(console.error);
-      if(message.mentions.users.size > 1) return message.reply(`ولد ما يصحلك الا حرمة وحدة كل مرة`).catch(console.error);
-       if(proposed === message.author) return message.reply('**خنتني ؟ **');
-        if(proposed === client.user) return message.reply('**تبي تتزوجني؟**')
+      if(!message.mentions.members.first()) return message.reply('لازم تطلب ايد وحدة').catch(console.error);
+      if(message.mentions.users.size > 1) return message.reply('ولد ما يصحلك الا حرمة وحدة كل مرة').catch(console.error);
+       if(proposed === message.author) return message.reply(**خنثى ؟ **);
+        if(proposed === client.user) return message.reply(** تبي تتزوجني؟ **);
               message.channel.send(**${proposed} 
  بدك تقبلي عرض الزواج من ${message.author}
  العرض لمدة 10 ثانية 
@@ -68,6 +68,7 @@ message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
 
   }
 });
+
 
 
 
