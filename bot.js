d2713c37c47f17f7aebf250bd5109ca6e275e84a
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-20 18:42:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,41 +32,12 @@ client.login(process.env.BOT_TOKEN);
 
 
 
-client.on('message', message => {
-if (!points[message.author.id]) points[message.author.id] = {
-    points: 50,
-  };
-if (message.content.startsWith(prefix + 'رياضيات')) { 
-    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
 
-const type = require('./read/read.json'); 
-const item = type[Math.floor(Math.random() * type.length)]; 
-const filter = response => { 
-    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-message.channel.send('**لديك 15 كلمه **').then(msg => {
-    let embed = new Discord.RichEmbed()
-    .setColor('#000000')
-    .setFooter("رياضيات  | NoobBot", 'https://cdn.discordapp.com/avatars/439427357175185408/3eb163b7656922ebc9e90653d50231f1.png?size=2048')
-    .setDescription(`**اكتب عاصمه: ${item.type}**`)
 
-    msg.channel.sendEmbed(embed).then(() => {
-        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-        .then((collected) => {
-        message.channel.send(`${collected.first().author} ✅ **الاجابه صحيحه**`); //mohamed192837465#7033صاحب الكود
 
-        console.log(`[Typing] ${collected.first().author} typed the word.`);
-            let won = collected.first().author; 
-            points[won.id].points++;
-          })
-          .catch(collected => { 
-            message.channel.send(`:x: **لا يوجد احد كتب الاجابه الصحيحه**`);
-            console.log(`[Typing] ماحد فكك الكلمه `);
-          })
-        })
-    })
-}
-});
+
+
+
 
 client.on('message', message => {
 if (!points[message.author.id]) points[message.author.id] = {
@@ -335,7 +306,6 @@ client.on("message", message => {
 ❖$عقاب | لعبه عقاب
 ❖$hack | لعبه الهكر مع ذكر اسمك للي هكرته
 ❖$hac-2 | لعبه الهكر من دون ذكر اسمك للي هكرته
-❖$رياضيات | لعبه رياضيات
 ❖$عواصم | لعبه عواصم
 ❖$فكك | لعبه فكك
 `)
