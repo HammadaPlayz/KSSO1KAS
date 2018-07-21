a34c8c3fd0050c04ab8fd00f582a23771c7c8ea1
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-22 00:10:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1992,41 +1992,6 @@ client.on('message' , async (message) => {
   
   
 
-client.on('message', async message => {
-     if (message.content.startsWith("$daily")) {
-    let cooldown = 8.64e+7,
-    amount = 250
-
-    let lastDaily = await db.fetch(`lastDaily_${message.author.id}`)
-    try {
-    db.fetch(`userBalance_${message.member.id}`).then(bucks => {
-    if(bucks == null){
-        db.set(`userBalance_${message.member.id}`, 50)}
-
-    else if (lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0) {
-        let timeObj = ms(cooldown - (Date.now() - lastDaily))
-
-        let lastDailyEmbed = new Discord.RichEmbed()
-        .setAuthor(`Next Daily`)
-        .setColor('#ffffff')
-        .setDescription(`You sucessfully collected this, you must wait to collect next dily. Time Left: **${timeObj}**!`)
-        .setFooter('Requested By ' + message.author.tag, message.author.avatarURL)
-        message.channel.send(lastDailyEmbed)
-    } else {
-        db.set(`lastDaily_${message.author.id}`, Date.now());
-        db.add(`userBalance_${message.member.id}`, amount).then(i => {
-          var discord = require('discord.js')
-          var embed = new Discord.RichEmbed()
-          .setTitle('Todays Daily')
-          .setDescription(`Sucessfully collected :dollar:$${amount}`)
-          .setColor('#ffffff')
-          .setFooter('Requested By ' + message.author.tag, message.author.avatarURL)
-          message.channel.send(embed);
-        })}
-    fs.writeFile('./credits' ,JSON.stringify(credits), (err) => {
-		})} catch(err) {console.log(err)}
-}
-});
 
 
 
