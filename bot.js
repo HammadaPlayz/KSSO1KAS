c86528894815c812fb3cb8925937f316a0f02dd4
mohamed192837465 <40711768+mohamed192837465@users.noreply.github.com>
2018-07-21 20:07:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2210,7 +2210,6 @@ var user = message.mentions.users.first() || message.author;
 
 }
 });
-const ms = require('ms')
 client.on('message', async message => {
    if(message.content.startsWith(prefix + "daily")) {
     let cooldown = 8.64e+7,
