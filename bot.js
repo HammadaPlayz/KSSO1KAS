4722b787513836fe8dd900b898d711f3bcff2017
proman123123 <40711768+IPROMAN123@users.noreply.github.com>
2018-07-23 00:59:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -404,6 +404,7 @@ client.on('message', message => {
   if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('$bc')){
+	 if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('**ماعندك صلاحيه لا تحاول**');
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
 var bc = new
