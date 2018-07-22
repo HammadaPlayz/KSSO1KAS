3dc85710fe56ee114f13193312f086a9ce157a65
proman123123 <40711768+IPROMAN123@users.noreply.github.com>
2018-07-22 16:18:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ client.on('message', async msg => {
 	const args = msg.content.split(' ');
 	const searchString = args.slice(1).join(' ');
 	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
-	const serverQueue = queue.get(msg.guild.id);
+	const queue = queue.get(msg.guild.id);
 	let command = msg.content.toLowerCase().split(" ")[0];
 	command = command.slice(prefix.length)
 	if (command === `play`) {
