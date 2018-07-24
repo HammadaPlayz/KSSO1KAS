d9fb5df4b7030c76cb42112eabb450b9f1b9cd9d
proman123123 <40711768+IPROMAN123@users.noreply.github.com>
2018-07-24 15:20:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,54 +35,6 @@ client.login(process.env.BOT_TOKEN);
 
 
 
-const translate = require('google-translate-api');
-const Langs = ['afrikaans','albanian','amharic','arabic','armenian','azerbaijani','bangla','basque','belarusian','bengali','bosnian','bulgarian','burmese','catalan','cebuano','chichewa','chinese simplified','chinese traditional','corsican','croatian','czech','danish','dutch','english','esperanto','estonian','filipino','finnish','french','frisian','galician','georgian','german','greek','gujarati','haitian creole','hausa','hawaiian','hebrew','hindi','hmong','hungarian','icelandic','igbo','indonesian','irish','italian','japanese','javanese','kannada','kazakh','khmer','korean','kurdish (kurmanji)','kyrgyz','lao','latin','latvian','lithuanian','luxembourgish','macedonian','malagasy','malay','malayalam','maltese','maori','marathi','mongolian','myanmar (burmese)','nepali','norwegian','nyanja','pashto','persian','polish','portugese','punjabi','romanian','russian','samoan','scottish gaelic','serbian','sesotho','shona','sindhi','sinhala','slovak','slovenian','somali','spanish','sundanese','swahili','swedish','tajik','tamil','telugu','thai','turkish','ukrainian','urdu','uzbek','vietnamese','welsh','xhosa','yiddish','yoruba','zulu'];
-
-client.on('message' , async (message) => {
-       if(message.content.startsWith(prefix + "tran")) {
-              let args = message.content.split(" ").slice(1);
-
-  if (args[0] === undefined) {
-
-    const embed = new Discord.RichEmbed()
-    .setColor("FFFFFF")
-    .setDescription("من فضلك قم بكتابه لغه");
-
-    return message.channel.send(embed);
-
-  } else {
-
-    if (args[1] === undefined) {
-
-      return message.channel.send('**من فضلك قم بكتابه كلمه`');
-
-    } else {
-
-      let transArg = args[0].toLowerCase();
-(edited)
- args = args.join(' ').slice(prefix.length);
-      let translation;
-
-      if (!Langs.includes(transArg)) return message.channel.send(`**لا يوجد لغه بـهذه الاسم**`);
-      args = args.slice(transArg.length);
-
-      translate(args, {to: transArg}).then(res => {
-
-        const embed = new Discord.RichEmbed()
-        .setDescription(res.text)
-        .setFooter(`english -> ${transArg}`)
-        .setColor(`RANDOM`);
-        return message.channel.send(embed);
-
-      });
-
-    }
-
-  }
-
-}
-});
-
 client.on('message', message => {
             if (message.content.startsWith(prefix + "bot")) {
      let embed = new Discord.RichEmbed()
